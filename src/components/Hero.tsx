d4cd903f8b763034d353adf800384f6fa945b552
chrisli5 <chrisli41@gmail.com>
2021-10-19 15:13:54 -0700

    refactor: remove unnecessary elements in components

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,21 +1,13 @@
-import React from "react";
-import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 
-const Hero = () => (
-  <Container>
-    <Row>
-      <Col className="px-0 col-12">
-        <Image
-          src="../../images/bayc-mutant-hero.jpeg"
-          fluid
-          className="px-0"
-        />
-      </Col>
-    </Row>
-  </Container>
+const Hero = (): JSX.Element => (
+  <Row>
+    <Col>
+      <Image src="../../images/bayc-mutant-hero.jpeg" fluid />
+    </Col>
+  </Row>
 );
 
 export default Hero;
